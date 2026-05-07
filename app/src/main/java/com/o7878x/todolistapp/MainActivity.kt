75af58c649876343696eaab840d842eb10010d29
o7878x <o8080x@outlook.com>
2026-05-07 14:45:33 +0800

    fix: show status bar

--- app/src/main/java/com/o7878x/todolistapp/MainActivity.kt
+++ app/src/main/java/com/o7878x/todolistapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.o7878x.todolistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
@@ -22,7 +21,6 @@ import kotlinx.coroutines.flow.map
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TodoListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
